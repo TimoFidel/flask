27f6b5d6430a5451c07ab64c7eab122821571602
TimoFidel <61385072+TimoFidel@users.noreply.github.com>
2020-09-08 08:28:22 +0530

    created basic app

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
+from pymongo import MongoClient
+from datetime import datetime
 
 app=Flask(__name__)
+client=MongoClient("mongodb://localhost:27017/")
+db=client["mydatabase"]
+col=db["posts"]
+
+site=[
+    {"web_link":"https://www.w3schools.com","comment":" THE WORLD'S LARGEST WEB DEVELOPER SITE","added_by":"asif","added_at":datetime.now()},
+    {"web_link":"http://www.amazon.in","comment":" Online Shopping India - Buy mobiles, laptops, cameras, books, watches, apparel, shoes and e-Gift Cards. Free Shipping & Cash on Delivery ...","added_by":"asif","added_at":datetime.now()},
+    {"web_link":"http://www.twitter.com","comment":"Twitter to find the latest news and world events faster. Find popular people, hashtags and photos for any topic you can imagine.","added_by":"asif","added_at":datetime.now()}
+]
+
+
+col.insert_many(site)
+
+sites=col.find().limit(3)
+
 
 @app.route('/')
-def hello():
-    return "hello World"
+def post():
+    return render_template('index.html',posts=sites)
 
 if __name__=="__main__":
     app.run(debug=True)
